ef73cd7210cea93542fe0aab256eca62fef96ce4
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2024-02-11 14:29:59 +0600

    css added

---
+++ script.js
@@ -0,0 +1,10 @@
+// const item = document.querySelectorAll(".link");
+
+// item.forEach((element, i) => {
+//   element.addEventListener("click", () => {
+//     item.forEach((element) => {
+//       element.classList.remove("active");
+//     });
+//     item[i].classList.add("active");
+//   });
+// });
